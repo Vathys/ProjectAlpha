f7974e6eedf323185db4ff1762db9c5ead68958e
Vathys <deepdesai00@gmail.com>
2019-09-18 17:34:35 -0500

    Update ProjectHandler.java

--- Server/src/com/alpha/pre_process/gui/ProjectHandler.java
+++ Server/src/com/alpha/pre_process/gui/ProjectHandler.java
@@ -1,6 +1,54 @@
 package com.alpha.pre_process.gui;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 public class ProjectHandler
 {
+     private File projectConfig;
+     private HashMap<String, File> files;
      
+     public ProjectHandler(File parent)
+     {
+          files = new LinkedHashMap<String, File>();
+          
+          for(File f : parent.listFiles())
+          {
+               if(f.getName().contains(".project"))
+               {
+                    projectConfig = f;
+               }
+               else if(parent.isDirectory())
+               {
+                    files.putAll(expandDirectory(f));
+               }
+          }
+     }
+     
+     private HashMap<String, File> expandDirectory(File directory)
+     {
+          HashMap<String, File> files = new LinkedHashMap<String, File>();
+          
+          for(File f : directory.listFiles())
+          {
+               if(f.isFile())
+               {
+                    try
+                    {
+                         files.put(f.getCanonicalPath(), f);
+                    } catch (IOException e)
+                    {
+                         e.printStackTrace();
+                    }
+               }
+               else
+               {
+                    files.putAll(expandDirectory(f));
+               }
+          }
+          return files;
+     }
 }
